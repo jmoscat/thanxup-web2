191ef3a7f87a84e60d6d32f475b40a863e27fa6f
Jorge Moscat <jorgemoscat@gmail.com>
2013-10-14 00:49:27 +0200

    Minor fixes

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -2,7 +2,7 @@ class StatusController < ApplicationController
   before_filter :authenticate_user!
   def index
   	id = current_user.venues.last.venue_thnx_id
-  	@name = current_user.venues.last.venue_name
+  	@name = Stat.where(venue_id: id).last.name
   	@cupons_created = Stat.where(venue_id: id).last.cupon_created
   	@cupons_used = Stat.where(venue_id: id).last.cupon_used
   	@visits = Stat.where(venue_id: id).last.visits
